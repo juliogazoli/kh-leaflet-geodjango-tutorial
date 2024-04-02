bd4a8de3d0771aad3b56dfb4160d3a0ca7eebf19
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2024-04-02 10:56:00 -0300

    leaflet map class

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 window.onload = init;
 
 function init() {
-    const mymap = L.map('mapid', {
+
+    const mapElement = document.getElementById('mapid')
+
+    const mymap = L.map(mapElement, {
         center: [48, 14],
-        zoom: 4,
+        zoom: 5,
+        minZoom: 4,
         layers: [
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             })
         ]
-})
+    })
+
+    mymap.on('resize', function (e) {
+        console.log('The map has been resized')
+    })
+
+    mymap.on('resize', function (e) {
+        mymap.flyTo([0, 0])
+    })
+
+    console.log(mymap.getPanes().tilePane)
 }
