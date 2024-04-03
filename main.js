08ef111beffc9326f1e3b4315691f7717c08b87d
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2024-04-03 12:22:24 -0300

    map controls

--- main.js
+++ main.js
@@ -4,24 +4,30 @@ function init() {
 
     const mapElement = document.getElementById('mapid')
 
+    const stadiaMaps = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+        attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        ext: 'png'
+    })
+
+    const openStreetMapStandard = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
+
     const mymap = L.map(mapElement, {
         center: [48, 14],
         zoom: 5,
         minZoom: 4,
-        layers: [
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            })
-        ]
+        layers: [openStreetMapStandard]
     })
 
-    mymap.on('resize', function (e) {
-        console.log('The map has been resized')
-    })
+    const baseLayers = {
+        '<b>openStreetMapStandard</b>': openStreetMapStandard,
+        'stadiaMaps': stadiaMaps
+    }
 
-    mymap.on('resize', function (e) {
-        mymap.flyTo([0, 0])
-    })
-
-    console.log(mymap.getPanes().tilePane)
+    const layerControls = L.control.layers(baseLayers, {}, {
+        collapsed: false,
+        position: 'topright'
+    }).addTo(mymap)
 }
