70707731c40cb885cc8ce050c584e88137dcb928
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2024-04-05 12:11:03 -0300

    raster layers

--- main.js
+++ main.js
@@ -2,18 +2,21 @@ window.onload = init;
 
 function init() {
 
+    // HTML element
     const mapElement = document.getElementById('mapid')
+
+    // Basemaps
+    const openStreetMapStandard = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
 
     const stadiaMaps = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
         attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         ext: 'png'
     })
 
-    const openStreetMapStandard = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    })
-
+    // Leaflet map object
     const mymap = L.map(mapElement, {
         center: [48, 14],
         zoom: 5,
@@ -21,12 +24,28 @@ function init() {
         layers: [openStreetMapStandard]
     })
 
+    // Basemap Object
     const baseLayers = {
         '<b>openStreetMapStandard</b>': openStreetMapStandard,
         'stadiaMaps': stadiaMaps
     }
 
-    const layerControls = L.control.layers(baseLayers, {}, {
+    // Overlays
+    const perthBaseMapImage = './Data/Perth_Image.png'
+    const perthBaseMapBounds = [[-41.30724736172, 112.5164607767221], [-22.42303778128779, 135.0225544458992]]
+    const imagePerthOverlay = L.imageOverlay(perthBaseMapImage, perthBaseMapBounds)
+
+    // Overlay object
+    const overlayerLayers = {
+        'Perth image': imagePerthOverlay
+    }
+
+    // mymap.on('click', function (event) {
+    //     console.log(event.latlng)
+    // })
+
+    // Layer control
+    const layerControl = L.control.layers(baseLayers, overlayerLayers, {
         collapsed: false,
         position: 'topright'
     }).addTo(mymap)
